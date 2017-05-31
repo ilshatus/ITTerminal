0c892d4bf0a01e7559ff8a1e9ac8aa14d639f8a5
Alevs <ac1298@mail.ru>
2017-05-31 15:36:04 +0300

    add barcode and nfc card readers

---
+++ BarcodeReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+namespace WpfApplication1
+{
+    public class BarcodeReader
+    {
+        Action<string> barCodeCallback;
+        StringBuilder mScanData = new StringBuilder();
+        KeyConverter mScanKeyConverter = new KeyConverter();
+        KeyEventHandler handler;
+        Window formWindow;
+
+        public BarcodeReader(Window window)
+        {
+            formWindow = window;
+        }
+
+        public void Read(Action<string> callback)
+        {
+            barCodeCallback = callback;
+            handler = new KeyEventHandler(MainWindow_PreviewKeyDown);
+            formWindow.PreviewKeyDown += handler;
+        }
+
+        void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {    
+          if (e.Key == Key.Return)
+            {
+                barCodeCallback(mScanData.ToString());
+                mScanData.Clear();
+                formWindow.PreviewKeyDown -= handler;
+            }
+            else
+            {
+                string xChar = mScanKeyConverter.ConvertToString(e.Key);
+                mScanData.Append(xChar);
+            }
+
+        }
+
+    }
+}
---
+++ CardReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.IO.Ports;
+
+namespace WpfApplication1
+{
+    class CardReader
+    {
+        const string COM_PORT = "COM4";
+        private string nfc_code;
+        public delegate void NfcReadCallback(string code);
+        private NfcReadCallback callback;
+        private SerialPort serialPort;
+
+        public CardReader()
+        {
+            SerialPort m_mySerialPort;
+            m_mySerialPort = new SerialPort(COM_PORT);
+
+            m_mySerialPort.BaudRate = 9600;
+            m_mySerialPort.Parity = Parity.None;
+            m_mySerialPort.StopBits = StopBits.One;
+            m_mySerialPort.DataBits = 8;
+            m_mySerialPort.Handshake = Handshake.None;
+            m_mySerialPort.RtsEnable = true;
+            m_mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            m_mySerialPort.Open();
+
+            serialPort = m_mySerialPort;
+        }
+
+        public void CloseConnection()
+        {
+            serialPort.Close();
+        }
+
+        public void Read(NfcReadCallback cb)
+        {
+            callback = cb;
+        }
+
+        private void DataReceivedHandler(
+                    object sender,
+                    SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string s = sp.ReadExisting().TrimEnd('\r', '\n');
+            nfc_code += s;
+            if (nfc_code.Length == 8)
+            {
+                callback?.Invoke(nfc_code);
+                callback = null;
+                nfc_code = "";
+            }
+        }
+    }
+}
